5187da5c55be6626558e92f4dfa8d58c2f36907d
杨欢 <sy.yanghuan@gmail.com>
2022-08-10 22:43:53 +0800

    update

---
+++ createnewfiles.bat
@@ -0,0 +1,10 @@
+if exist bin rd bin /s /q
+set compilerDir=bin\CSharp.lua
+git clone https://github.com/yanghuan/CSharp.lua.git %compilerDir%
+set compilerPublishDir=%compilerDir%\bin\Publish
+dotnet publish %compilerDir% --configuration Release --output %compilerPublishDir%
+set filesDir=bin\files
+mkdir %filesDir%
+xcopy %compilerPublishDir%\System.xml %filesDir% /y
+xcopy %compilerPublishDir%\CSharp.lua.dll %filesDir% /y
+xcopy %compilerPublishDir%\Microsoft.CodeAnalysis* %filesDir% /y
