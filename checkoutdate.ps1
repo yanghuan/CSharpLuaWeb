ac4776d28ee653ee898c89334297bb87ae5cc59d
杨欢 <sy.yanghuan@gmail.com>
2022-08-10 21:05:21 +0800

    update

--- checkoutdate.ps1
+++ checkoutdate.ps1
@@ -1,7 +1,9 @@
-$response = Invoke-RestMethod -Uri "https://api.github.com/repos/yanghuan/CSharp.lua/branches/master"
-$lastCommitDate = [datetime]($response.commit.commit.author.date)
-echo lastCommitDate: $lastCommitDate
-$lastBuildDate = [datetime](Get-ItemProperty -Path ./files/CSharp.lua.dll -Name LastWriteTime).lastwritetime
-echo lastBuildDate: $lastBuildDate
-$isOutDate = $lastCommitDate -le $lastBuildDate
+$compilerResponse = Invoke-RestMethod -Uri "https://api.github.com/repos/yanghuan/CSharp.lua/branches/master"
+$lastCompilerCommitDate = [datetime]($compilerResponse.commit.commit.author.date)
+echo lastCompilerCommitDate: $lastCompilerCommitDate
+
+$webResponse = Invoke-RestMethod -Uri "https://api.github.com/repos/yanghuan/CSharpLuaWeb/commits?path=files%2FCSharp.lua.dll&page=1&per_page=1"
+$lastWebCommitDate = [datetime]($webResponse[0].commit.author.date)
+echo lastWebCommitDate: $lastWebCommitDate
+$isOutDate = $lastWebCommitDate -le $lastCompilerCommitDate
 echo "::set-output name=isOutDate::$isOutDate"
